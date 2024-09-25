d5bdeab965abbcf783ca65506e811b4330b4fdf7
fsiddiqui2 <49000400+fsiddiqui2@users.noreply.github.com>
2024-09-24 20:59:35 -0400

    Update dataloader.py
    defined function yahooData25

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,9 @@
 from qiskit_finance.data_providers import YahooDataProvider
-stocks = ['AAPL', 'MSFT', 'NVDA', 'GOOG', 'GOOGL', 'META', 'BRK-B', 'LLY', 'AVGO', 'TSLA', 'WMT', 'JPM', 'UNH', 'XOM', 'V', 'ORCL', 'MA', 'PG', 'COST', 'HD', 'JNJ', 'ABBV', 'NFLX', 'KO', 'BAC']
-data = YahooDataProvider(tickers=stocks,
-                 start=datetime.datetime(2023,1,1),
-                 end=datetime.datetime(2024,1,1))
-data.run()
+
+def yahooData25(y1, m1, d1, y2, m2, d2):
+    stocks = ['AAPL', 'MSFT', 'NVDA', 'GOOG', 'GOOGL', 'META', 'BRK-B', 'LLY', 'AVGO', 'TSLA', 'WMT', 'JPM', 'UNH', 'XOM', 'V', 'ORCL', 'MA', 'PG', 'COST', 'HD', 'JNJ', 'ABBV', 'NFLX', 'KO', 'BAC']
+    data = YahooDataProvider(tickers=stocks,
+                     start=datetime.datetime(y1,m1,d1),
+                     end=datetime.datetime(y2,m2,d2))
+    data.run()
+    return data
