e3d218d152a148b2f559e037a3a89f61634da3ed
Farhaan Siddiqui <fsiddiqui@Farhaans-MacBook-Pro.local>
2024-09-28 11:52:14 -0400

    delete extra

---
+++ .ipynb_checkpoints/dataloader-checkpoint.py
@@ -0,0 +1,7 @@
+from qiskit_finance.data_providers import YahooDataProvider
+from datetime import datetime
+def yahooData25(y1=2023, m1=1, d1=1, y2=2024, m2=1, d2=1):
+    stocks = ['AAPL', 'MSFT', 'NVDA', 'GOOG', 'GOOGL', 'META', 'BRK-B', 'LLY', 'AVGO', 'TSLA', 'WMT', 'JPM', 'UNH', 'XOM', 'V', 'ORCL', 'MA', 'PG', 'COST', 'HD', 'JNJ', 'ABBV', 'NFLX', 'KO', 'BAC']
+    data = YahooDataProvider(tickers=stocks, start=datetime(y1,m1,d1), end=datetime(y2,m2,d2))
+    data.run()
+    return data
